2def62ff59ad5ae17cb84a1e2540c184065435a5
Billy Li <billy.li.901@gmail.com>
2020-05-24 16:32:46 -0700

    cms env

---
+++ src/components/aboutComponent/AboutMe.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Title from '../Title';
+
+const AboutMe = () => (
+  <section>
+    <Title
+      SectionNumber="02"
+      title="About Me"
+      subtitle="Things that I like"
+      black
+    />
+
+  </section>
+);
+
+export default AboutMe;
